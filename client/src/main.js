1b3a424d9a1ed3588032d5c8523241ed46541836
Brian Graham <brianmgraham224@gmail.com>
2024-08-05 19:27:43 -0700

    added FileUploader component

--- client/src/main.js
+++ client/src/main.js
@@ -9,6 +9,7 @@ import Spinner from './components/Spinner.vue'
 import Alert from './components/Alert.vue'
 import ListView from './components/ListView.vue'
 import EditView from './components/EditView.vue'
+import FileImport from './components/FileUploader.vue'
 
 const app = createApp(App);
 
@@ -16,6 +17,7 @@ app.component('Spinner', Spinner);
 app.component('Alert', Alert);
 app.component('ListView', ListView)
 app.component('EditView', EditView)
+app.component('FileImport', FileImport)
 
 app.use(router);
 
